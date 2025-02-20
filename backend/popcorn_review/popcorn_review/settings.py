7296542058e23fd19699e9551ab2c74df8e99adb
Md Mohibullah <mdmohibullah352@gmail.com>
2025-02-20 16:54:46 +0600

    added api and search

--- backend/popcorn_review/popcorn_review/settings.py
+++ backend/popcorn_review/popcorn_review/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'popcorn_review.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
