fbe3d302455388f572edd520c4e0d6b78def61a7
Maruf Hossain <maruf.bshs@gmail.com>
2025-02-24 12:14:05 +0600

    added navbar and logo

--- popcorn_review/popcorn_review/settings.py
+++ popcorn_review/popcorn_review/settings.py
@@ -9,11 +9,16 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+# settings.py
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'reviews/static')]
 
 
 # Quick-start development settings - unsuitable for production
